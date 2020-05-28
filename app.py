a654903716d42c9abf2f47d85c2d424e1ca6f290
alpertucanberk <alper.tu.canberk@gmail.com>
2020-05-27 18:21:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -218,19 +218,19 @@ def change_room_count():
 	if request.method == "POST":
 		data = request.form
 		room = data["room"]
-		isConnecting = data["isConnecting"]
+		count = data["count"]
 
-		addition = -1
-		if(isConnecting == "true"):
-			addition = 1
+		# addition = -1
+		# if(isConnecting == "true"):
+		# 	addition = 1
 
-		print("new connection")
-		print(">>>" + room)
-		print(">>>>>>>>>")
+		# print("new connection")
+		# print(">>>" + room)
+		# print(">>>>>>>>>")
 
 		room_count = gtd(rooms_ref.where("name", "==", room).stream())[0]["count"]
 		rooms_ref.document(room).set({
-			"count": room_count + addition,
+			"count": count,
 			"name":room
 		})
 		return json.dumps({
